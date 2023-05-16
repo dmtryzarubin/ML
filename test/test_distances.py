7b79855aba07c7a493a6b88f0d1ca31f46fd34d7
Dmitry Zarubin <dmtryzarubin@mail.ru>
2023-05-16 22:08:37 +0300

    add manual_seed to distances test

--- test/test_distances.py
+++ test/test_distances.py
@@ -1,10 +1,13 @@
 import torch
 from ml.distance.functional import eucledian_dist, eucledian_dist_loops
 
+torch.manual_seed(2023)
+DEVICE = "cpu"
+
 
 def test_distance():
-    X = torch.randn(100, 2)
-    c = torch.randn(3, 2)
+    X = torch.randn(100, 2, device=DEVICE)
+    c = torch.randn(3, 2, device=DEVICE)
     dists1 = eucledian_dist(X, c)
     dists2 = eucledian_dist_loops(X, c)
     assert torch.allclose(dists1, dists2)
